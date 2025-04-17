027885ca14766473d4178dc0c86340948f11f61c
PixelVoid <Pixel0Void@gmail.com>
2025-04-17 15:23:56 +0430

    feat: add direction utility class

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class Direction
+{
+    public enum DirectionEnum
+    {
+        up, down, left, right, MAX
+    }
+
+    private static DirectionEnum m_Type;
+    public static DirectionEnum TypeDir => m_Type;
+
+    public static DirectionEnum GetRandomDirection()
+    {
+        m_Type = (DirectionEnum)Random.Range(0, (int)DirectionEnum.MAX);
+        return m_Type;
+    }
+}

e6fd8ddd2428d301d14e3f3e141955baeae2dce5
PixelVoid <Pixel0Void@gmail.com>
2025-04-17 17:04:46 +0430

    feat: add menu script, load game scene, instantiate board based on selected size in menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Dropdown SizeDropDown;
+
+    public void PlayGame()
+    {
+        int size = 0;
+        switch (SizeDropDown.value)
+        {
+            case 0: size = 3; break;
+            case 1: size = 4; break;
+            case 2: size = 5; break;
+            case 3: size = 6; break;
+            case 4: size = 7; break;
+            case 5: size = 8; break;
+            case 6: size = 9; break;
+            default:goto case 0;
+        }
+        PlayerPrefs.SetInt("Size", size);
+        SceneManager.LoadScene(1);
+    }
+}

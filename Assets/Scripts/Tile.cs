54c06b28dafbfcb4d7510144467519b4663d2495
PixelVoid <Pixel0Void@gmail.com>
2025-04-17 14:37:23 +0430

    feat: add tile script, tile prefab and tile sprite

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private int m_Number;
+    public int Number => m_Number;
+
+    private TextMesh m_NumTxt;
+
+    private void Awake()
+    {
+        m_NumTxt = GetComponentInChildren<TextMesh>();
+    }
+
+    public void SetNumber(int num)
+    {
+        m_Number = num;
+        m_NumTxt.text = m_Number.ToString();
+    }
+}

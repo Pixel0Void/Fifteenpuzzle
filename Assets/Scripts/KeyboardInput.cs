f1bf0f67564db3632892eaf9ae7242921e23aee7
PixelVoid <Pixel0Void@gmail.com>
2025-04-17 17:18:23 +0430

    feat: add keyboard input script, assign it to game manager object

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour
+{
+    private Board m_Board;
+
+    private void Awake()
+    {
+        m_Board = GameObject.FindWithTag("GameManager").GetComponent<Board>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            m_Board.MoveTile(Direction.DirectionEnum.left);
+        }
+        else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            m_Board.MoveTile(Direction.DirectionEnum.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            m_Board.MoveTile(Direction.DirectionEnum.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            m_Board.MoveTile(Direction.DirectionEnum.down);
+        }
+    }
+}
